767c461e31e5c0307d14de21df632470167e8f0d
Endzo Useri <CoffeeBlack7@protonmail.com>
2021-08-17 15:52:08 -0700

    Breaking down the code

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let index = 0;
+// let index = 0;
 
-function changeColors() {
-    let colors = ["red", "blue", "orange", "yellow", "green", "purple", "white", "chocolate"]
+// function changeColors() {
+//     let colors = ["red", "blue", "orange", "yellow", "green", "purple", "white", "chocolate"]
 
-    document.getElementsByTagName("body")[0].style.background = colors[index++];
+//     document.getElementsByTagName("body")[0].style.background = colors[index++];
 
-    if(index > colors.length - 1)
-      index = 0;
-}
+//     if(index > colors.length - 1)
+//       index = 0;
+// }
