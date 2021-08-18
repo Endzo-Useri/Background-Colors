bb74424d78b97363cd8e7f10569309768cc17856
Endzo Useri <CoffeeBlack7@protonmail.com>
2021-08-17 18:14:06 -0700

    Update JS

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ let index = 0;
 function changeColors() {
     let colors = ["red", "blue", "orange", "yellow", "green", "purple", "white", "chocolate"]
 
-    document.getElementsByTagName("body")[0].style.background = colors[index++];
+    document.body.style.background = colors[index++];
 
     if(index > colors.length - 1)
       index = 0;
