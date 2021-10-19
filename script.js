791b7b4079ac51360343fddb919ba178b6488ab6
endzo-useri <coffeeblack7@protonmail.com>
2021-10-18 18:37:27 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,12 @@
-// let index = 0;
-
-// function changeColors() {
-//     let colors = ["red", "blue", "orange", "yellow", "green", "purple", "white", "chocolate"]
-
-//     document.getElementsByTagName("body")[0].style.background = colors[index++];
-
-//     if(index > colors.length - 1)
-//       index = 0;
-// }
-
 let index = 0;
 
 function changeColors() {
     let colors = ["red", "blue", "orange", "yellow", "green", "purple", "white", "chocolate"]
 
+		// document.getElementsByTagName("body")[0].style.background = colors[index++];
     // Compliments of Tarek Visch
     document.body.style.background = colors[index++];
 
-    if(index > colors.length - 1)
+    if (index > colors.length - 1)
       index = 0;
 }
